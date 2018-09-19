ec31cbfff127b303dd0e64e5fd26c8c1c8e9c7b3
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-19 16:06:48 -0400

    Display area timer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,9 +5,10 @@ var incorrect = 0;
 var unanswered = 0;
 
 // Begin Game Function
-$(document).ready(function () {
+$(document).ready(function() {
 
   // Hide the Game and Final Stats
+  $("#triviaStartArea").hide();
   $("#triviaGameArea").hide();
   $("#triviaStatsArea").hide();
 
@@ -16,7 +17,26 @@ $(document).ready(function () {
     $("#triviaStartArea").hide();
     $("#triviaGameArea").show();
     $("#triviaStatsArea").hide();
+    return;
   }
   
+    // Create a Function for the Display Areas
+    function counter() {
+      timerCount--;
+      $("#timer").html(timerCount + " Seconds");
+      $("#doneButton").on("click", function() {
+        ("#triviaGameArea").hide();
+        timesUp();
+        ("#triviaStatsArea").show();
+        return;
+      });
+
+      if (count == -1) {
+        timesUp();
+        ("#triviaGameArea").hide();
+        ("#triviaStatsArea").show();
+      }
+
+    }
 
 })
