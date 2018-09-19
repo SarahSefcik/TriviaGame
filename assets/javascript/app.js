d41b5d7cc951f50c49f12ed7bb12c546983bb830
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-19 15:12:19 -0400

    Javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Create a timer, correct, incorrect, and unanswered variables
+var timerCount = 60;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
+// Begin Game Function
+$(document).ready(function () {
+
+  // Hide the Game and Final Stats
+  $("#triviaGameArea").hide();
+  $("#triviaStatsArea").hide();
+
+
+})
