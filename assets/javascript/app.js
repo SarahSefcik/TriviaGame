39f2f999ab769b4082587660a1342f3d43221f3e
Sarah Catherine Sefcik <sefciksa@gmail.com>
2018-09-21 23:07:12 -0400

    Generated Text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,16 @@ var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var trivia = [
-  { question: "What's your favorite color?", choices: ["red", "blue", "green", "yellow"], answer: "red" },
-  { question: "What's your favorite color?", choices: ["red", "blue", "green", "yellow"], answer: "red" },
+  { question: "What city do the Simpsons live in?", choices: ["Shelbyville", "Springfield", "Chicago", "Capital City"], answer: "Springfield" },
+  { question: "What is Bart doing at the beginning of every episode?", choices: ["Riding a bike", "Picking on Lisa", "Writing on a chalkboard", "Pretending to drive a car"], answer: "Writing on a chalkboard" },
+  { question: "When was the series premiere of the Simpsons?", choices: ["1989", "1991", "1987", "1998"], answer: "1989" },
+  { question: "What is the name of the bar that Bart prank calls?", choices: ["Kwik-E-Mart", "Mr. Burns'", "Moe's", "Flander's"], answer: "Moe's" },
+  { question: "How many sisters does Marg have?", choices: ["two", "three", "four", "none"], answer: "two" },
+  { question: "What are Marg's sisters names?", choices: ["Thelma & Louise", "Patty & Selma", "Connie, Bonnie & Patty", "Selma, Louise & Bonnie"], answer: "Patty & Selma" },
+  { question: "What's the name of the groundskeeper Bart feuds with?", choices: ["Groundskeeper Joe", "Groundskeeper Tom", "Groundskeeper Billie", "Groundskeeper Willie"], answer: "Groundskeeper Willie" },
+  { question: "In an episode, which President spanked Bart?", choices: ["Bill Clinton", "George H.W. Bush", "George W. Bush", "Ross Perot"], answer: "George H.W. Bush" },
+  { question: "Marg Simpson had many jobs, which of the following is not a job she had?", choices: ["Roller skate waitress", "Ventriloquist", "Assassin", "Bus driver"], answer: "Bus driver" },
+  { question: "What is Ned Flanders occupation?", choices: ["Owner of Leftorium", "Bible school teacher", "Priest", "Stay at home dad"], answer: "Owner of Leftorium" },
 ];
 
 
